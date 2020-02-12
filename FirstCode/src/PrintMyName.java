469b0a40b53afc8b5f56396714208955df878a14
user <user@10.42.33.130>
2020-02-12 09:02:14 +0300

    Last code

--- FirstCode/src/PrintMyName.java
+++ FirstCode/src/PrintMyName.java
@@ -4,6 +4,7 @@ public class PrintMyName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("My name is Nasser");
+System.out.println("My last name is alharthy");
 	}
 
 }
